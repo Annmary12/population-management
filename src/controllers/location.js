ee8be182137cfa51b363fcd353c10df3eb525e0d
Annmary12 <annmaryamaka@yahoo.com>
2019-05-08 12:28:00 +0100

    add get all location method

--- src/controllers/location.js
+++ src/controllers/location.js
@@ -36,6 +36,30 @@ class LocationController {
       })
     }
   }
+
+  static async get(req, res) {
+    try {
+      const options = {
+        page: req.query.page ? Number(req.query.page) : 1,
+        limit: 5
+      };
+      const data = await Location.paginate({}, options);
+
+      if (data.docs.length < 0)
+        return status(400).json({ message: 'Location not found!'});
+
+      return res.status(200).json({
+        data,
+        message: 'List of all locations'
+      });
+
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Error occured',
+        error
+      })
+    }
+  }
 }
 
 export default LocationController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,16 +3,17 @@ import LocationController from '../controllers/location';
 import Validation from '../middleware/validateInput';
 import checkLocationExist from '../middleware/checkLocation';
 
-const { create } = LocationController;
+const { create, get } = LocationController;
 const { inputValidation } = Validation;
 const route = Router();
 
-route.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Welcome to population management system API!!!'
-  })
-});
+// route.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Welcome to population management system API!!!'
+//   })
+// });
 
 route.post('/', inputValidation, checkLocationExist, create);
+route.get('/', get);
 
 export default route;
